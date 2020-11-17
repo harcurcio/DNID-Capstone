bf778f48dd13fabe5fab0286ba9621a3be06762e
Harlie Curcio <hmc26@pitt.edu>
2020-11-17 03:18:22 -0500

    faq page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 function menuClick(x) {
     x.classList.toggle("change");
 }
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+}
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+}
